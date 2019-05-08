c9b924ef920ab1a365394df7beba5f04a13dbee5
Bentley Davis <Bentley@BentleyDavis.com>
2019-05-07 21:05:26 -0500

    Initial commit with 11ty a two example bad arguments

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("CNAME");
+
+  eleventyConfig.addShortcode("maincss", function (test) {
+    var fs = require('fs');
+    var path = process.cwd();
+    var filePath = path + "\\main.css";
+    if (fs.existsSync(filePath)) {
+      var buffer = fs.readFileSync(filePath).toString();
+    }
+    return buffer;
+  });
+
+  return {
+    dir: {
+      output: "dist"
+    }
+  }
+};
+
